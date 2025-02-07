e5db976f7088b6917b3ad5b4bc5c112202be0038
mark <genuinepower123@gmail.com>
2025-02-08 01:48:53 +0800

    sd

--- src/app/components/product-layout/smallScreen/mainDish-product-layout.tsx
+++ src/app/components/product-layout/smallScreen/mainDish-product-layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { MdAddComment } from "react-icons/md";
+import { IoMdAddCircle } from "react-icons/io";
 
 type Data = {
     name: string;
@@ -9,14 +9,14 @@ type Data = {
     url: string;
 }
 
-const MainDishProductLayout: React.FC<Data> = ({name,price,url}) => {
+const MainDishProductLayout: React.FC<Data> = ({name,price,url,description}) => {
     return (
         <div 
             className='h-[190px] w-[100%] flex justify-center items-center rounded-tr-[20px] rounded-bl-[20px] overflow-hidden'
         >
             <div className='bg-[#cacaca] h-[97%] w-[97%] relative flex flex-col justify-between'>
                 <h2 
-                    className='relative font-poppins bg-[#000000] max-w-min px-2 text-white z-10'
+                    className='relative font-poppins bg-[#a39a81] max-w-min px-2 text-white z-10'
                 >{price}</h2>
                 <div className='absolute top-0 h-[80%] w-[100%] z-1 flex justify-center'>
                     <div className='relative h-full w-[90%]'>
@@ -27,9 +27,9 @@ const MainDishProductLayout: React.FC<Data> = ({name,price,url}) => {
                         />
                     </div>
                 </div>
-                <div className='flex justify-between items-center px-2 h-[20%] bg-[#979797]'>
-                    <h2 className='font-poppins text-white'>{name}</h2>
-                    <MdAddComment />
+                <div className='flex justify-between items-center px-2 h-[20%] bg-[#fff1cc33]'>
+                    <h2 className='font-poppins text-[#504f4f]'>{name}</h2>
+                    <IoMdAddCircle />
                 </div>
             </div>
         </div>
