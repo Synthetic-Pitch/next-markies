d8ec198cad3e79b4027558e5d506a122aa39bfa7
mark <genuinepower123@gmail.com>
2025-02-11 00:50:28 +0800

    main

--- src/app/components/product-map/desert.tsx
+++ src/app/components/product-map/desert.tsx
@@ -3,20 +3,12 @@ import { cookies } from 'next/headers';
 import Sort from './Sort';
 
 
-type Data = {
-  data:{
-    name:string;
-    price:string;
-    description:string
-    url:string;
-    _id:string;
-  }
-}
+
 
 async function GetProducts() {
   'use server'
   const response = await fetch('https://next-markies.vercel.app/api/get/desert')
-  const data:Data[] = await response.json()
+  const data= await response.json()
   return data.data
 }
 
