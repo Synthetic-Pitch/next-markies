fe50341bdb0f2d89e5538b795a43d60d6ed9c316
mark <genuinepower123@gmail.com>
2025-02-10 23:35:23 +0800

    main

--- src/app/components/product-map/desert.tsx
+++ src/app/components/product-map/desert.tsx
@@ -1,39 +1,11 @@
-import { cookies } from "next/headers"
-import { Suspense } from "react"
-import MainDishProductLayout from "../product-layout/smallScreen/mainDish-product-layout"
+import React from 'react';
 
-
-  type Data = {
-    name:string
-    price:string
-    description:string
-    url:string
-  }
-  
-  async function GetProducts() {
-    const response = await fetch('https://next-markies.vercel.app/api/get/mainDish')
-    const data = await response.json()
-    console.log(data);
-    
-    return(
-      <div className="grid grid-cols-1 sxs:grid-cols-2 xs:grid-cols-3 px-1 py-2 justify-center">
-        {
-          data.map((product: Data, index: number) => (
-            <MainDishProductLayout key={index} name={product.name} price={product.price} description={product.description} url={product.url}/>
-          ))
-        }
-      </div>
-    )
-  }
-  
-  export default function Desert() {
-    cookies();
+const Desert = () => {
     return (
-      <>
-        <h2>Head</h2>
-        <Suspense fallback={<>Loading...</>}>
-          <GetProducts />
-        </Suspense>
-      </>
+        <div>
+            asd
+        </div>
     );
-  }
+};
+
+export default Desert;
