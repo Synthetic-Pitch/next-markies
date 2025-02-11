32478de980dd941eaf32dad0fbae677248374768
mark <genuinepower123@gmail.com>
2025-02-11 12:05:17 +0800

    main

--- src/app/components/product-map/beverage.tsx
+++ src/app/components/product-map/beverage.tsx
@@ -1,6 +1,8 @@
+import { cookies } from "next/headers";
 import Sort from "./Sort";
 
 export default async function Beverage() {
+    cookies();
     const response = await fetch('https://next-markies.vercel.app/api/get/beverage');
     const data = await response.json();
     
