81986227fddd1eed2b41e1a4264e1afba45d4111
mark <genuinepower123@gmail.com>
2025-02-11 11:25:43 +0800

    main

--- src/app/components/product-map/desert.tsx
+++ src/app/components/product-map/desert.tsx
@@ -9,7 +9,7 @@ async function GetProducts() {
   'use server'
   const response = await fetch('https://next-markies.vercel.app/api/get/desert')
   const data= await response.json()
-  return data.data
+  return data
 }
 
 export default async function Desert ()  {
